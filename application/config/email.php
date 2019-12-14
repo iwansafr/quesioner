3424fccfc2b31d2f9b97d245d52ec9b1ea721426
iwansafr <iwansafr@gmail.com>
2019-12-14 07:07:16 +0700

    mastering

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['protocol']     = 'smtp';
+$config['smtp_host']    = 'ssl://mail.mandesa.co.id';
+$config['smtp_port']    = '465';
+$config['smtp_timeout'] = '7';
+$config['smtp_user']    = 'iwan@mandesa.co.id';
+$config['smtp_pass']    = "fi_271091";
+$config['charset']      = 'utf-8';
+$config['newline']      = "\r\n";
+$config['mailtype']     = 'html'; // or html
+$config['validation']   = TRUE; // bool whether to validate email or not
